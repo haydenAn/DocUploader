4a717e701e434292a674f958e00e9896df63474d
Hayden Rahn <anhayzzing33@gmail.com>
2020-12-07 11:13:21 -0600

    report-page ui-2

--- UploadApp-SPA/src/app/search/search.component.ts
+++ UploadApp-SPA/src/app/search/search.component.ts
@@ -1,45 +1,40 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from "@angular/animations";
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css'],
+  selector: "app-search",
+  templateUrl: "./search.component.html",
+  styleUrls: ["./search.component.css"],
   animations: [
-    trigger(
-      'inOutAnimation', 
-      [
-        transition(
-          ':enter', 
-          [
-            style({ height: 0}),
-            animate('0.5s ease-out', 
-                    style({ height: 300}))
-          ]
-        ),
-        transition(
-          ':leave', 
-          [
-            style({ height: 300 }),
-            animate('0.5s ease-in', 
-                    style({ height: 0 }))
-          ]
-        )
-      ]
-    )
-  ]
+    trigger("inOutAnimation", [
+      transition(":enter", [
+        style({ height: 0, opacity: 0 }),
+        animate("0.5s ease-out", style({ height: 260, opacity: 1 })),
+      ]),
+      transition(":leave", [
+        style({ height: 260, opacity: 1 }),
+        animate("0.5s ease-in", style({ height: 0, opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
 export class SearchComponent implements OnInit {
-  @Output() sendSearch = new EventEmitter<{keyword: string, filter: any}>();
+  @Output() sendSearch = new EventEmitter<{ keyword: string; filter: any }>();
   panelOpenState = false;
 
-  keyword: string = '';
-  email:string = '';
-  firstName:string = '';
-  lastName:string = '';
-  title: string ='';
-  company:string='';
-  description: string = '';
+  keyword: string = "";
+  email: string = "";
+  firstName: string = "";
+  lastName: string = "";
+  title: string = "";
+  company: string = "";
+  description: string = "";
 
   filterDict = {};
 
@@ -48,14 +43,12 @@ export class SearchComponent implements OnInit {
   viewed = false;
   agreed = false;
 
-  constructor() { 
-  }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   searchReport() {
-    console.log('Searching for keyword ', this.keyword, this.email);
+    console.log("Searching for keyword ", this.keyword, this.email);
     this.filterDict["EmailAddress"] = this.email;
     this.filterDict["FirstName"] = this.firstName;
     this.filterDict["LastName"] = this.lastName;
@@ -64,31 +57,40 @@ export class SearchComponent implements OnInit {
     this.filterDict["Description"] = this.description;
 
     let copy = Object.assign({}, this.filterDict);
-    this.sendSearch.emit({keyword:this.keyword, filter :copy});
+    this.sendSearch.emit({ keyword: this.keyword, filter: copy });
+    this.panelOpenState = false;
   }
 
   onCheckboxChange(value: string, ischecked: boolean) {
-    console.log('radio button', value, ' is checked=', ischecked);
+    console.log("radio button", value, " is checked=", ischecked);
     this.filterDict["Status"] = value.toString();
   }
 
   clearFilters() {
-    this.keyword = '';
+    this.keyword = "";
+    this.email= "";
+    this.firstName= "";
+    this.lastName= "";
+    this.title= "";
+    this.company= "";
+    this.description= "";
+
     this.sent = false;
     this.resent = false;
     this.viewed = false;
     this.agreed = false;
+
+    this.panelOpenState = false;
 
     for (var key in this.filterDict) {
       delete this.filterDict[key];
     }
     Object.assign(this.filterDict, {});
     //don't change this line
-    this.sendSearch.emit({keyword:this.keyword, filter:null});
+    this.sendSearch.emit({ keyword: this.keyword, filter: null });
   }
 
-   toggleOpenState(){
-     this.panelOpenState = !this.panelOpenState;
-   }
-
+  toggleOpenState() {
+    this.panelOpenState = !this.panelOpenState;
+  }
 }
